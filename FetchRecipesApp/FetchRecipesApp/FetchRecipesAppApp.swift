d79fe67aee004ddd1257398c7ab22a7c3a9ee8aa
Jon-Luke <jonlukejenkins2@gmail.com>
2024-11-30 13:26:04 -0600

    initial commit

---
+++ FetchRecipesApp/FetchRecipesApp/FetchRecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesAppApp.swift
+//  FetchRecipesApp
+//
+//  Created by Jon-Luke Jenkins on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipesApp/FetchRecipesAppTests/FetchRecipesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesAppTests.swift
+//  FetchRecipesAppTests
+//
+//  Created by Jon-Luke Jenkins on 11/30/24.
+//
+
+import Testing
+@testable import FetchRecipesApp
+
+struct FetchRecipesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
